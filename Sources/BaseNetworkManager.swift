df340f41e60dce705dd77143556ed466beeffd7f
Rafael M Mudafort <rafmudaf@gmail.com>
2017-08-11 22:52:36 -0600

    add simple cookie handling

--- Sources/BaseNetworkManager.swift
+++ Sources/BaseNetworkManager.swift
@@ -2,14 +2,50 @@ import Foundation
 import Alamofire
 import HTMLReader
 
-
 class BaseNetworkManager {
   
     init () {
       // TODO initialization
     }
     
-    func getHtml<T: BaseModel>(url: String, headers: [String: String]?, parameters: [String: AnyObject]?, completion: @escaping (_ data: T?, _ response: URLResponse?, _ error: Error?) -> Void) {
+    private func set(cookies: [HTTPCookie], for url: URL) {
+        Alamofire.SessionManager.default.session.configuration.httpCookieStorage?.setCookies(cookies, for: url, mainDocumentURL: nil)
+        
+    }
+    
+    public func get(cookies host: URL) -> [HTTPCookie]? {
+        return Alamofire.SessionManager.default.session.configuration.httpCookieStorage?.cookies
+    }
+    
+    public func establishConnection(url: String, headers: [String: String]?, parameters: [String: AnyObject]?, completion: @escaping (_ data: [HTTPCookie]?, _ response: URLResponse?, _ error: Error?) -> Void) {
+        
+        print(url)
+        
+        var stayAlive = true
+        
+        Alamofire.request(url, method: .get, parameters: parameters)
+            .validate { request, response, data in
+                return .success
+            }
+            .response { response in
+                guard let header = response.response?.allHeaderFields as? [String: String], let url = response.request?.url else {
+                    completion(nil, response.response, response.error)
+                    return
+                }
+                
+                let cookies = HTTPCookie.cookies(withResponseHeaderFields: header, for: url)
+                self.set(cookies: cookies, for: url)
+                completion(cookies, response.response, response.error)
+                stayAlive = false
+        }
+        
+        let runLoop = RunLoop.current
+        while stayAlive && runLoop.run(mode: RunLoopMode.defaultRunLoopMode, before: Date(timeIntervalSinceNow: 0.1)) {
+            // Run, run, run
+        }
+    }
+    
+    public func getHtml<T: BaseModel>(url: String, headers: [String: String]?, parameters: [String: AnyObject]?, completion: @escaping (_ data: T?, _ response: URLResponse?, _ error: Error?) -> Void) {
         
         print(url)
         
@@ -31,10 +67,15 @@ class BaseNetworkManager {
                     return
                 }
                 
+//                print(htmlAsString)
                 let doc = HTMLDocument(string: htmlAsString)
-                
-                print(htmlAsString)
-                
+                let e = doc.bodyElement?.treeEnumerator()
+//                for ee in e! {
+//                    print(ee)
+//                }
+//                print(doc.bodyElement?.treeEnumerator())
+//                print(doc.children)
+//                print(doc.nodes(matchingSelector: "tr"))
                 
                 stayAlive = false
             }
--- Sources/NetworkManager.swift
+++ Sources/NetworkManager.swift
@@ -4,9 +4,40 @@ class NetworkManager: BaseNetworkManager {
     
     let baseURL = "https://www.southwest.com/"
     
-    override init () { }
+    let headers = [
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "en-US,en;q=0.8",
+        "Cache-Control": "max-age=0",
+        "Connection": "keep-alive",
+        "Host": "www.southwest.com",
+        "Upgrade-Insecure-Requests": "1",
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36",
+        "Content-Type": "application/json; charset=utf-8"
+    ]
+    
+    var cookies: String?
+    
+    override init () {
+        // TODO
+    }
     
     static let sharedInstance = NetworkManager()
+    
+    func establishConnection() {
+        establishConnection(url: baseURL + "flight", headers: headers, parameters: nil) { (data, response, error) in
+            guard let cookies = data else {
+                // TODO handle error
+                return
+            }
+            self.cookies = cookies.flatMap{String(describing: $0)}.joined(separator: " ")
+        }
+    }
+    
+    func get(endpoint: String, parameters: [String: AnyObject]?, body: [String: String]?, completion: @escaping (_ data: HtmlModel?, _ response: URLResponse?, _ error: Error?) -> Void) {
+        let url = baseURL+endpoint
+        getHtml(url: url, headers: headers, parameters: parameters, completion: completion)
+    }
     
     func getFlightSelectFlight(headers: [String: String]?, parameters: [String: AnyObject]?, completion: @escaping (_ data: HtmlModel?, _ response: URLResponse?, _ error: Error?) -> Void) {
       let url = baseURL+"flight/select-flight.html"
