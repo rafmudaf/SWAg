1e9028bae8ec58f3bf02c00a380ea5a5c82a540e
Rafael M Mudafort <rafmudaf@gmail.com>
2017-08-15 19:46:32 -0600

    chain the requests to avoid asynchronous problems

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,28 +1,31 @@
 import Foundation
 
-// establish the persistent connect; i.e. cookies
-NetworkManager.sharedInstance.establishConnection()
-
-// submit the search for the flight
-let body = [
-    "originCode": "DEN",
-    "destinationCode": "MSY",
-    "returnCode": "DEN",
-    "outboundDate": "08/12/2017",
-    "inboundDate": "08/14/2017",
-    "tripType": "false",
-    "travelOnAirTran": "false",
-    "noModalFor": "null",
-    "lowfareSearch": "undefined"
-]
-let parameters = [
-    "displayOnly": "",
-    "int": "HOMEQBOMAIR"
-]
-
-
-NetworkManager.sharedInstance.get(endpoint: "/flight/select-flight.html", parameters: parameters as [String : AnyObject], body: body) { (data, resposne, error) in
-    guard let html = data else {
-        return
+func searchForFlight() {
+    // submit the search for the flight
+    let parameters = [
+        "displayOnly": "",
+        "int": "HOMEQBOMAIR"
+    ]
+    let body = [
+        "originCode": "DEN",
+        "destinationCode": "MSY",
+        "returnCode": "DEN",
+        "outboundDate": "09/01/2017",
+        "inboundDate": "09/04/2017",
+        "tripType": "false",
+        "travelOnAirTran": "false",
+        "noModalFor": "null",
+        "lowfareSearch": "undefined"
+    ]
+    NetworkManager.sharedInstance.get(endpoint: "flight/select-flight.html", parameters: parameters, body: body) { (data, response, error) in
+        guard let html = data else {
+            return
+        }
     }
 }
+
+// establish the persistent connect; i.e. cookies
+NetworkManager.sharedInstance.establishConnection() { (data, response, error) in
+    searchForFlight()
+}
+
