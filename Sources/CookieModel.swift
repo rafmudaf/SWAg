26e9108752bbba3bdd98c4ffa006d6290a13f534
Rafael M Mudafort <rafmudaf@gmail.com>
2017-08-15 19:47:09 -0600

    add a cookie abstraction model

---
+++ Sources/CookieModel.swift
@@ -0,0 +1,46 @@
+import Foundation
+
+class CookieModel: BaseModel {
+    
+    private var string: String?
+    // private var url: URL?
+    // private var title: String?
+    // private var link: URL?
+    // private var author: String?
+    // private var description: String?
+    // private var image: String?
+    // private var articles = [NewsArticleModel]()
+    
+    override func loadDataFrom(dictionary: NSDictionary) {
+        
+        dictionaryRepresentation = dictionary
+        
+        // status = dictionary.object(forKey: "status") as? String ?? nil
+        //
+        // if let feed = dictionary.object(forKey: "source") as? NSDictionary {
+        //
+        //     if let urlString = feed.object(forKey: "url") as? String {
+        //         url = URL(string: urlString)
+        //     } else {
+        //         url = nil
+        //     }
+        //
+        //     title = feed.object(forKey: "title") as? String ?? nil
+        //
+        //     if let urlString = feed.object(forKey: "link") as? String {
+        //         link = URL(string: urlString)
+        //     } else {
+        //         link = nil
+        //     }
+        //
+        //     author = feed.object(forKey: "author") as? String ?? nil
+        //     description = feed.object(forKey: "description") as? String ?? nil
+        //     image = feed.object(forKey: "image") as? String ?? nil
+        // }
+        //
+        // for article in dictionary.object(forKey: "items") as! NSArray {
+        //     let art = NewsArticleModel(dictionary: article as! NSDictionary)
+        //     articles.append(art)
+        // }
+    }
+}
